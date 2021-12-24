7ed49fe944d15834c5f1583be4479f71d5874a77
Thanh Nguyen <vnscriptkid@gmail.com>
2021-12-24 23:30:29 +0700

    feat(coffees): not found exception

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
@@ -17,7 +17,11 @@ export class CoffeesService {
   }
 
   findOne(id: number) {
-    return this.coffees.find((c) => c.id === id);
+    const coffee = this.coffees.find((c) => c.id === id);
+
+    if (!coffee) throw new NotFoundException(`Coffee #${id} not found`);
+
+    return coffee;
   }
 
   update(id: number, data: Partial<Coffee>) {
