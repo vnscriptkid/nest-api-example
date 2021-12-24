d901e14959c495b01614c7bddf71a9656a987eae
Thanh Nguyen <vnscriptkid@gmail.com>
2021-12-25 00:27:05 +0700

    feat(coffees): validate over updateDto

--- src/coffees/dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto.ts
@@ -1,5 +1,15 @@
+import { IsOptional, IsString } from 'class-validator';
+
 export class UpdateCoffeeDto {
+  @IsString()
+  @IsOptional()
   readonly name?: string;
+
+  @IsString()
+  @IsOptional()
   readonly brand?: string;
+
+  @IsString()
+  @IsOptional()
   readonly flavors?: string[];
 }
