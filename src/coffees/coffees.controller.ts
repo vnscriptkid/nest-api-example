96a09577059ddc52174011521e6a3c41e07d0035
Thanh Nguyen <vnscriptkid@gmail.com>
2021-12-25 00:40:13 +0700

    feat(validations): add configs

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -30,6 +30,7 @@ export class CoffeesController {
   @Post()
   create(@Body() createCoffeeDto: CreateCoffeeDto) {
     return this.coffeesService.create(createCoffeeDto);
+    return createCoffeeDto;
   }
 
   @Patch(':id')
