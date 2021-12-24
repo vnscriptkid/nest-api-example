12274fc61d914fda0bd9f27be4a194caf60755ca
Thanh Nguyen <vnscriptkid@gmail.com>
2021-12-25 00:36:04 +0700

    refactor(coffees): reduce duplication in updateDto

--- src/coffees/dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto.ts
@@ -1,15 +1,4 @@
-import { IsOptional, IsString } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateCoffeeDto } from './create-coffee.dto';
 
-export class UpdateCoffeeDto {
-  @IsString()
-  @IsOptional()
-  readonly name?: string;
-
-  @IsString()
-  @IsOptional()
-  readonly brand?: string;
-
-  @IsString()
-  @IsOptional()
-  readonly flavors?: string[];
-}
+export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) {}
