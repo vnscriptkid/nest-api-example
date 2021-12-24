96160b0c838f0ae9d85d7c18da83a5f31c24f4a9
Thanh Nguyen <vnscriptkid@gmail.com>
2021-12-24 17:39:38 +0700

    feat: update coffee

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -15,5 +15,10 @@ export class CoffeesController {
   @Post()
   create(@Body() body) {
     return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `updates coffee #${id} with data: ${JSON.stringify(body, null, 2)}`;
   }
 }
